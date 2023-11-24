f7f074ac70f46a6c8470ef8f1431fb18b55bfd45
whs <hswongac@gmail.com>
2023-11-24 15:31:02 +0800

    Test

--- flake.nix
+++ flake.nix
@@ -1,40 +1,49 @@
 {
-    description = "zkSync development shell";
-    inputs = {
-        stable.url = "github:NixOS/nixpkgs/nixos-22.11";
-    };
-    outputs = {self, stable}: {
-        packages.x86_64-linux.default =
-        with import stable { system = "x86_64-linux"; };
-        pkgs.mkShell {
-            name = "zkSync";
-            src = ./.;
-            buildInputs = [
-                docker-compose
-                nodejs
-                yarn
-                axel
-                libclang
-                openssl
-                pkg-config
-                postgresql
-                python3
-                solc
-            ];
+  description = "zkSync development shell";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  inputs.fenix.url = "github:nix-community/fenix";
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+  }: {
+    packages.x86_64-linux.default = with import nixpkgs {system = "x86_64-linux";};
+      pkgs.mkShell {
+        name = "zkSync";
+        src = ./.;
+        buildInputs = [
+          docker-compose
+          nodejs
+          yarn
+          axel
+          libclang
+          openssl
+          pkg-config
+          postgresql
+          python3
+          solc
+          (with fenix.packages.${system};
+            combine [
+              default.toolchain
+              targets.x86_64-unknown-linux-musl.latest.rust-std
+            ])
+        ];
 
-            # for RocksDB and other Rust bindgen libraries
-            LIBCLANG_PATH = lib.makeLibraryPath [ libclang.lib ];
-            BINDGEN_EXTRA_CLANG_ARGS = ''-I"${libclang.lib}/lib/clang/${libclang.version}/include"'';
+        # for RocksDB and other Rust bindgen libraries
+        LIBCLANG_PATH = lib.makeLibraryPath [libclang.lib];
+        BINDGEN_EXTRA_CLANG_ARGS = ''-I"${libclang.lib}/lib/clang/16/include"'';
 
-            shellHook = ''
-                export ZKSYNC_HOME=$PWD
-                export PATH=$ZKSYNC_HOME/bin:$PATH
-            '';
+        shellHook = ''
+          export ZKSYNC_HOME=$PWD
+          export PATH=$ZKSYNC_HOME/bin:$PATH
+        '';
 
-            # hardhat solc requires ld-linux
-            # Nixos has to fake it with nix-ld
-            NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [];
-            NIX_LD = builtins.readFile "${stdenv.cc}/nix-support/dynamic-linker";
-        };
-    };
+        # hardhat solc requires ld-linux
+        # Nixos has to fake it with nix-ld
+        NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [];
+        NIX_LD = builtins.readFile "${stdenv.cc}/nix-support/dynamic-linker";
+      };
+  };
 }
